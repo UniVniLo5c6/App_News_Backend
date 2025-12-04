2a21b464277fa6caf2048a0cc1dddc4148cf209e
UniVniLo5c6 <23520863@gm.uit.edu.vn>
2025-12-05 02:07:28 +0700

    sửa articleController

--- src/controllers/articleController.js
+++ src/controllers/articleController.js
@@ -16,14 +16,7 @@ const User = require('../models/user');
  */
 exports.list = async (req, res) => {
   try {
-    const { rssitemid } = req.query;
-    const where = {};
-
-    if (rssitemid) {
-      where.rssItemId = rssitemid;
-    }
-
-    const articles = await Article.findAll({ where, include: [{ model: User, as: 'author', attributes: ['id', 'name', 'email'] }], order: [['createdAt', 'DESC']] });
+    const articles = await Article.findAll({ include: [{ model: User, as: 'author', attributes: ['id', 'name', 'email'] }], order: [['createdAt', 'DESC']] });
     return res.json(articles);
   } catch (err) {
     console.error(err);
